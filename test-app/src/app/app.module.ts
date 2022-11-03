abd811ab9c613191658910bc08333af1d654abae
Muriel Anoma <37190323+murielanoma@users.noreply.github.com>
2022-11-03 08:41:52 +0530

    Muriel_3rd_Nov

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewComponentComponent
+    NewComponentComponent,
+    NavigationBarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
